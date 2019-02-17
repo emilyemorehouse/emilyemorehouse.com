47e373c818333f7365b849150c9a3dd927744c90
Emily Morehouse <emilyemorehouse@gmail.com>
2019-02-17 16:37:38 -0700

    🏗 Update Footer - Add page links; update styles; remove unused styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,49 @@
 import React, { Component } from "react";
 import { Link } from "gatsby";
+import { Flex, Text } from "rebass";
+import styled from "styled-components";
+
 import UserLinks from "../UserLinks/UserLinks";
-import "./Footer.css";
+
+const StyledLink = styled(Link)`
+  color: black;
+  padding: 5px;
+
+  &:link,
+  &:visited {
+    color: black;
+  }
+  &:hover {
+    background-color: black;
+    color: white;
+  }
+`;
 
 class Footer extends Component {
   render() {
     const { config } = this.props;
-    const url = config.siteRss;
-    const { copyright } = config;
-    if (!copyright) {
-      return null;
-    }
-    return (
-      <footer className="footer">
-        <UserLinks config={config} labeled />
-        <div className="notice-container">
-          <h4>{copyright}</h4>
 
-          <Link to={url}>
-            <button>Subscribe</button>
-          </Link>
-          <h4>
-            Based on{" "}
-            <a href="https://github.com/Vagr9K/gatsby-advanced-starter">
-              Gatsby Advanced Starter
-            </a>.
-          </h4>
-        </div>
+    return (
+      <footer
+        style={{
+          justifyContent: "center",
+          alignContent: "center",
+          padding: "10px 5px 5px"
+        }}
+      >
+        <Flex alignItems="center" justifyContent="center" mb={20}>
+          {config.visiblePages.map(page => (
+            <StyledLink to={`/${page}`}>
+              <Text fontFamily="mono">{page}</Text>
+            </StyledLink>
+          ))}
+        </Flex>
+
+        <UserLinks config={config} labeled />
+
+        <Text fontFamily="mono" fontSize={[0]} mb={20} textAlign="center">
+          <h4>{config.copyright}</h4>
+        </Text>
       </footer>
     );
   }
