83ee6a64461ba9487d10be832c3ceaae9384e6c4
Emily Morehouse <emilyemorehouse@gmail.com>
2019-02-17 16:37:16 -0700

    💄 Add ThemeProvider to Layout

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import Helmet from "react-helmet";
+import { ThemeProvider } from "styled-components";
+
 import config from "../../data/SiteConfig";
 import "./index.css";
+
+const theme = {
+  colors: {
+    blue: "#07c",
+    lightgray: "#f6f6ff"
+  },
+  fonts: {
+    sans: "Menlo, monospace",
+    mono: "Menlo, monospace"
+  }
+};
 
 export default class MainLayout extends React.Component {
   render() {
     const { children } = this.props;
+
     return (
       <div>
         <Helmet>
           <meta name="description" content={config.siteDescription} />
         </Helmet>
-        {children}
+        <ThemeProvider theme={theme}>
+          <div>{children}</div>
+        </ThemeProvider>
       </div>
     );
   }
