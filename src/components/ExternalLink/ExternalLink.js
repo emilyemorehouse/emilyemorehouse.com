ab512ce2d4a72b48a42cc264682b18b67bb8caf2
Emily Morehouse <emilyemorehouse@gmail.com>
2019-02-17 23:41:40 -0700

    🚑 Ensure that color props for ExternalLink work with default values

--- src/components/ExternalLink/ExternalLink.js
+++ src/components/ExternalLink/ExternalLink.js
@@ -4,23 +4,23 @@ import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const StyledLink = styled(Link)`
-  color: ${props => (props.color !== undefined ? props.color : '#ad774e')};
+  color: ${props => props.color};
 
   &:link,
   &:visited {
-    color: ${props => (props.color !== undefined ? props.color : '#ad774e')};
+    color: ${props => props.color};
   }
   &:hover {
-    background-color: ${props => (props.color !== undefined ? props.color : '#ad774e')};
-    color: ${props => (props.altColor !== undefined ? props.altColor : 'white')};
-    padding: ${props => (props.icon !== undefined ? '0px' : '2px')};
+    background-color: ${props => props.color};
+    color: ${props => props.altColor};
+    padding: ${props => (props.icon ? '0px' : '2px')};
   }
 `
 
 const ExternalLink = ({ altColor, color, icon, style, title, url }) => (
   <StyledLink
-    altColor={altColor}
-    color={color}
+    altColor={altColor || 'white'}
+    color={color || '#ad774e'}
     href={`/${url}`}
     icon={icon}
     style={style}
