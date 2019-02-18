bb95865342e23b1a4d1334053928a44838dfda20
Emily Morehouse <emilyemorehouse@gmail.com>
2019-02-17 19:19:32 -0700

    💄⬆️ Add font-awesome packages and library configuration; Update user links to properly use icons

--- src/components/UserLinks/UserLinks.jsx
+++ src/components/UserLinks/UserLinks.jsx
@@ -1,17 +1,36 @@
 import React, { Component } from 'react'
+import { Link } from 'rebass'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styled from 'styled-components'
+
 import './UserLinks.css'
+
+const StyledLink = styled(Link)`
+  color: black;
+  padding: 5px;
+
+  &:link,
+  &:visited {
+    color: black;
+  }
+  &:hover {
+    background-color: black;
+    color: white;
+  }
+`
 
 class UserLinks extends Component {
   getLinkElements() {
-    const { config, labeled } = this.props
+    const { config } = this.props
     const { userLinks } = config
 
     return userLinks.map(link => (
-      <button key={link.label} href={link.url}>
-        {labeled ? link.label : ''}
-      </button>
+      <StyledLink key={link.label} href={link.url} target="_blank">
+        <FontAwesomeIcon icon={link.iconClassName} />
+      </StyledLink>
     ))
   }
+
   render() {
     const { userLinks } = this.props.config
     if (!userLinks) {
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { ThemeProvider } from 'styled-components'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 import config from '../../data/SiteConfig'
 import './index.css'
+
+library.add(fab, faEnvelope)
 
 const theme = {
   colors: {
