88eede5d38a9f31f33ce5644eb692e71213c9dec
Emily Morehouse <emilyemorehouse@gmail.com>
2019-02-17 23:03:47 -0700

    🏗 Add about component; remove unused styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,50 @@
 import React, { Component } from 'react'
-import './About.css'
+import { Box, Flex, Text } from 'rebass'
+import ExternalLink from '../../components/ExternalLink/ExternalLink'
 
 class About extends Component {
   render() {
     return (
-      <div className="about">
-        <h1>Edit About component or pages/about.jsx to include your information.</h1>
-      </div>
+      <Flex alignItems="center" justifyContent="center">
+        <Box>
+          <Text
+            fontFamily="mono"
+            mt={30}
+            style={{ width: '80vw', maxWidth: 600 }}
+            textAlign="center"
+          >
+            I'm located in Denver, CO as the Cofounder and Director of Engineering at{' '}
+            <ExternalLink url="https://www.cuttlesoft.com" title="Cuttlesoft" />, a software
+            development company focused on building beauifully designed, highly scalable custom
+            software. I'm also a Python Core Developer and help maintain{' '}
+            <ExternalLink url="https://www.github.com/axios/axios" title="Axios" />
+          </Text>
+
+          <Text
+            fontFamily="mono"
+            mt={30}
+            style={{ width: '80vw', maxWidth: 600 }}
+            textAlign="center"
+          >
+            I graduated in the summer of 2014 from Florida State University with degrees in
+            Theatre, Criminology, and Computer Science as well as minoring in Mathematics and
+            Psychology (yeah, I was a little busy). Since then I've dabbled in graduate level
+            courses and dove into open source contributing while deepening my level of experience
+            with programming and running a business.
+          </Text>
+
+          <Text
+            fontFamily="mono"
+            mt={30}
+            style={{ width: '80vw', maxWidth: 600 }}
+            textAlign="center"
+          >
+            I'm interested in pursuing further research in the realm of programming language
+            theory, particularly formal verification and parallelization, as well as application
+            development with OSS.
+          </Text>
+        </Box>
+      </Flex>
     )
   }
 }
