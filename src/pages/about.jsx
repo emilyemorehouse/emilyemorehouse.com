0f896f5ff177ba155a9463203a3cf2e6e1605a73
Emily Morehouse <emilyemorehouse@gmail.com>
2019-02-17 23:05:22 -0700

    🏗 Add About page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,17 +1,28 @@
 import React, { Component } from 'react'
 import Helmet from 'react-helmet'
-import Layout from '../layout'
+import { Box, Flex, Heading } from 'rebass'
+
 import About from '../components/About/About'
+import SEO from '../components/SEO/SEO'
+import Layout from '../layout'
 import config from '../../data/SiteConfig'
 
 class AboutPage extends Component {
   render() {
     return (
       <Layout>
-        <div className="about-container">
-          <Helmet title={`About | ${config.siteTitle}`} />
-          <About />
-        </div>
+        <Helmet title={`About | ${config.siteTitle}`} />
+        <SEO />
+
+        <Flex
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: 'calc(100vh - 140px)' }}
+        >
+          <Box mt={80}>
+            <About />
+          </Box>
+        </Flex>
       </Layout>
     )
   }
