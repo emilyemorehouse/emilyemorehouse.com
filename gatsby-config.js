fbac0d0d2ed9af5988df7ff277f694ffbd9d32d7
Emily Morehouse <emily@cuttlesoft.com>
2019-12-30 17:28:47 -0700

    🚑 Add gatsby-plugin-styled-components to fix initial style load

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,9 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-lodash',
     {
+      resolve: 'gatsby-plugin-styled-components',
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'assets',
