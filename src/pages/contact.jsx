da9dbfa454db372f0d54e8715548d9bdd78e0a3f
Frank Valcarcel <frank@cuttlesoft.com>
2020-02-25 21:40:21 -0700

    ✨ Add Contact page with email-send CTAs; Update email (e.morehouse@cuttlesoft.com) and copyright (2020) (#2)
    * add a Contact page and update email link
    
    * actually add the Contact page this time
    
    * Apply suggestions from code review
    
    Co-authored-by: Emily Morehouse <emilyemorehouse@gmail.com>

---
+++ src/pages/contact.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+import Helmet from 'react-helmet'
+import { Box, Card, Flex, Heading, Text } from 'rebass'
+
+import SEO from '../components/SEO/SEO'
+import Layout from '../layout'
+import config from '../../data/SiteConfig'
+
+const ListItem = ({
+    eventName,
+    onClick,
+  }) => (
+    <Card
+      bg='rgba(142, 249, 243, 0.8)'
+      borderColor="black"
+      border={2}
+      my={3}
+      onClick={onClick}
+      p={3}
+      style={{'cursor': 'pointer'}}
+    >
+      <Heading fontSize={[2, 3, 4]} fontFamily="mono">
+        {eventName}
+      </Heading>
+    </Card>
+)
+
+const sendEmail = (cta) => {
+  let subject = null
+  switch (cta) {
+    case 'ask':
+      subject = '[Question] emilyemorehouse.com'
+      break
+    
+      case 'schedule':
+      subject = '[Schedule Consult] emilyemorehouse.com'
+      break
+    
+      case 'hello':
+      subject = '[👋 Hello] emilyemorehouse.com'
+      break
+    
+      default:
+      subject = '[Inquiry] emilyemorehouse.com'
+      break
+  }
+
+  window.location = `mailto:e.morehouse@cuttlesoft.com?subject=${subject}`;
+}
+
+class ContactPage extends Component {
+  render() {
+    return (
+      <Layout>
+        <Helmet title={`Contact | ${config.siteTitle}`} />
+        <SEO />
+
+        <Flex
+          alignItems="center"
+          justifyContent="center"
+          p={24}
+          style={{ minHeight: 'calc(100vh - 140px)' }}
+        >
+          <Box mt={80}>
+            <Heading mb={40} fontFamily="mono" fontSize={[5, 6]} textAlign="center">
+              Contact Me
+            </Heading>
+            <div>
+                <ListItem
+                    eventName="Ask Me A Question"
+                    onClick={() => sendEmail('ask')}
+                />
+                <ListItem
+                    eventName="Schedule A Consultation"
+                    onClick={() => sendEmail('schedule')}
+                />
+                <ListItem
+                    eventName="Say Hello!"
+                    onClick={() => sendEmail('hello')}
+                />
+            </div>
+          </Box>
+        </Flex>
+      </Layout>
+    )
+  }
+}
+
+export default ContactPage
